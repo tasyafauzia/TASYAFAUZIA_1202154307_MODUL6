ccdbe358a12295e59d260f908d2299f28d0acbdb
tasyafauzia <tasyafauzia97@gmail.com>
2018-04-01 22:50:33 +0700

    push keenam kali

---
+++ app/src/main/java/com/tasyafauzia/android/tasyafauzia_1202154307_modul6/Fragment/MyPostsFragment.java
@@ -0,0 +1,83 @@
+package com.tasyafauzia.android.tasyafauzia_1202154307_modul6.Fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+
+import com.tasyafauzia.android.tasyafauzia_1202154307_modul6.Adapter.PostAdapter;
+import com.tasyafauzia.android.tasyafauzia_1202154307_modul6.MainActivity;
+import com.tasyafauzia.android.tasyafauzia_1202154307_modul6.Model.Post;
+import com.tasyafauzia.android.tasyafauzia_1202154307_modul6.R;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MyPostsFragment extends Fragment{
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private ArrayList<Post> listPosts;
+
+    Query database;
+
+    public MyPostsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //initialize firebaseauth instance
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.activity_my_post, container, false);
+
+        recyclerView = view.findViewById(R.id.rv_myposts);
+        recyclerView.setHasFixedSize(true);
+
+        database = FirebaseDatabase.getInstance().getReference(MainActivity.table_1).orderByChild("userID").equalTo(mAuth.getCurrentUser().getUid());
+        listPosts = new ArrayList<>();
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                listPosts.clear();
+
+                for (DataSnapshot posts : dataSnapshot.getChildren()){
+                    Post post = posts.getValue(Post.class);
+                    listPosts.add(post);
+                }
+                recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+                PostAdapter postAdapter = new PostAdapter(getContext(), listPosts);
+                recyclerView.setAdapter(postAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/tasyafauzia/android/tasyafauzia_1202154307_modul6/Model/User.java
@@ -0,0 +1,43 @@
+package com.tasyafauzia.android.tasyafauzia_1202154307_modul6.Model;
+
+/**
+ * Created by Tasya Fauzia on 31/03/18.
+ */
+
+public class User {
+    String userID;
+    String username;
+    String email;
+
+    public User(){}
+
+    public User(String userID, String username, String email) {
+        this.userID = userID;
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
